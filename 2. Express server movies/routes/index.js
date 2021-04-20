0293b94f7530217199dee4077cd8ce50705c7ad6
ericanthony <ericanthonywu89@gmail.com>
2021-04-21 05:11:11 +0700

    apply logging to router

--- 2. Express server movies/routes/index.js
+++ 2. Express server movies/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const {mysqlLogging} = require("../middleware/loggingMiddleware");
 const {searchMovies, detailMovie} = require("../controller/apiController");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/search', searchMovies);
+router.get('/search', mysqlLogging, searchMovies);
 
-router.get('/detail/:id', detailMovie)
+router.get('/detail/:id', mysqlLogging, detailMovie)
 
 module.exports = router;
