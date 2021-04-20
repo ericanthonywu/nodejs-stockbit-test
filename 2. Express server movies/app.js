c749d49ebd5559c62ca41d445a72dab6cedf190b
ericanthony <ericanthonywu89@gmail.com>
2021-04-21 03:16:58 +0700

    simplify app.js

--- 2. Express server movies/app.js
+++ 2. Express server movies/app.js
@@ -4,8 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const {render404Error} = require("./middleware/errorHandler/errorHandlerMiddleware");
 
 const app = express();
 
@@ -22,23 +21,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req,res,next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(render404Error);
 
 module.exports = app;
