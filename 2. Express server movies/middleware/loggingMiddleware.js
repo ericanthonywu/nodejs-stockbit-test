a2e0c8e54e5cf9bd573ab18893f312f1a592b07e
ericanthony <ericanthonywu89@gmail.com>
2021-04-21 03:21:01 +0700

    initing API logging middleware

---
+++ 2. Express server movies/middleware/loggingMiddleware.js
@@ -0,0 +1,10 @@
+/**
+ * Logging API to Database
+ *
+ * @param {Request<ParsedQs>} req
+ * @param {Response|ServerResponse} res
+ * @param {NextFunction} next
+ */
+exports.mysqlLogging = (req,res,next) => {
+
+}
